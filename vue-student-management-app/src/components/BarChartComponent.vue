c129ed5d2bcd7d63ce3fbae351f63fe96e144a44
bscrisp <brandon.s.crisp@gmail.com>
2021-11-10 16:02:09 -0600

    Create BarChartComponent.vue

---
+++ vue-student-management-app/src/components/BarChartComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <canvas ref="myChart"></canvas>
+</template>
+
+<script>
+import Chart from "chart.js";
+export default {
+  props: {
+    label: {
+      type: Array,
+    },
+    chartData: {
+      type: Array,
+    },
+  },
+  async mounted() {
+    console.log(this.chartData);
+    await new Chart(this.$refs.myChart, {
+      type: "bar",
+      data: {
+        labels: this.label,
+        datasets: [
+          {
+            label: "Enrollment",
+            backgroundColor: "rgba(144,238,144 , 0.9 )",
+            data: this.chartData,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+  },
+};
+</script>
