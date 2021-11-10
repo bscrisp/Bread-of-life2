72b5a7d27edd14c79245eee831a7ec4144530d76
bscrisp <brandon.s.crisp@gmail.com>
2021-11-10 16:02:23 -0600

    Create PlanetChart.vue

---
+++ vue-student-management-app/src/components/PlanetChart.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- canvas element for chart -->
+    <canvas id="planet-chart"></canvas>
+  </div>
+</template>
+
+
+<script>
+import Chart from 'chart.js'
+import planetChartData from '../planet-data.js'
+
+export default {
+  name: 'PlanetChart',
+  data() {
+    return {
+      planetChartData: planetChartData
+    }
+  },
+  //establish Chart object after mounting the component
+  mounted() {
+    const ctx = document.getElementById('planet-chart');
+    new Chart(ctx, this.planetChartData);
+  }
+}
+</script>
